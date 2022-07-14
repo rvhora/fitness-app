03dd9ba2d1872e4312c7816bb97079d3b858e552
rvhora <rupeshvhora1988@gmail.com>
2022-07-14 14:22:37 +1000

    added new project

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+// import { Audio } from 'react-loader-spinner'
+import React from 'react';
+import { Stack } from '@mui/material';
+
+const Loader = () => (
+  <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
+    <p>Loading</p>
+  </Stack>
+);
+
+export default Loader;
